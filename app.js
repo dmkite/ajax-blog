89b2e135aa4260cf7b97816b25eb8e011e2b730c
Dylan <kite.d92@gmail.com>
2018-11-07 15:13:59 -0800

    added cors

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express()
 const port = process.env.PORT || 3000
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.use('/posts', require('./routes/posts'))
 
